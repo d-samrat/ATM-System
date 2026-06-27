6f2f4dfbc1ce4740c6c1b5a8bf6bc27890736d07
d-samrat <samrat4002@hotmail.com>
2026-06-27 23:47:08 +0530

    cleared main branch

--- Account Service/account-service/src/main/java/com/telusko/accountservice/model/CreateAccountRequest.java
+++
@@ -1,9 +0,0 @@
-package com.telusko.accountservice.model;
-
-import lombok.Data;
-
-@Data
-public class CreateAccountRequest {
-    private String holderName;
-    private long authId;
-}
--- Transaction Service/transaction-service/src/main/java/com/telusko/transactionservice/model/DepositRequest.java
+++
@@ -1,9 +0,0 @@
-package com.telusko.transactionservice.model;
-
-import lombok.Data;
-
-@Data
-public class DepositRequest {
-    private long accNo;
-    private long amount;
-}
